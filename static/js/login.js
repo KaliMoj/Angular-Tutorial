f99152f980e619054ac1b7fc3293203b6a190743
KaliMoj <c.obsidian@gmail.com>
2016-03-04 23:01:53 -0500

    Added task view
    Additional task view added after login. Will likely need to modify the
    API returning the full task rather than its ID upon creation.

--- static/js/login.js
+++ static/js/login.js
@@ -1,46 +1,68 @@
-var userTasks = angular.module('userTasks', ["ui.router"]).controller('userController', 
-["$http", UserController]);
+var userTasks = angular.module('userTasks', ['ui.router'])
+    .controller('userController', ['$http', '$state', '$stateParams', UserController]);
 
-function UserController($http) {
+function UserController($http, $state, $stateParams) {
     var that = this;
-    that.firstName = "";
-    that.lastName = "";
-    that.email = "";
+    if ($stateParams.data) {
+        that.Id = $stateParams.data.id;
+        that.firstName = $stateParams.data.firstName;
+        that.lastName = $stateParams.data.lastName;
+        that.email = $stateParams.data.email;
+        that.tasks = $stateParams.data.tasks;         
+    }
     that.register = function() {
-        $http.post('http://localhost:8080/user', 
-            { firstName: that.firstName, 
-              lastName: that.lastName, 
-              email: that.email })
+        $http.post('http://localhost:8080/user', { 
+                firstName: that.firstName,
+                lastName: that.lastName, 
+                email: that.email 
+            })
             .then(function(response) {
                 console.log("Success ", response);  
                 that.Id = response.data;      
-            }, 
-            function(err) {
-                console.log(err);
-            });               
-    };
+            }, ErrorCallback);
+    }
+    that.login = function() {
+        $http.get('http://localhost:8080/user/' + that.Id)
+        .then(function(response) {
+            console.log("Success ", response);
+            $state.go('myTasks.list', { data: response.data });
+        }, ErrorCallback);
+    }
+    that.addTask = function(description) {
+        $http.post('http://localhost:8080/user/' + that.Id + '/tasks', { 
+            description: description
+        })
+        .then(function(response) {
+            console.log("Success ", response); 
+            that.tasks.push(response.data);  
+        }, ErrorCallback);
+    }
 }
 
 userTasks.config(function($stateProvider, $urlRouterProvider) {
-  //
-  // For any unmatched url, redirect to /login
-  $urlRouterProvider.otherwise("/login");
-  //
-  // Now set up the states
-  $stateProvider
-    .state('login', {
-      url: "/login",
-      templateUrl: "partials/login.html"
-    })
-    .state('myTasks', {
-      url: "/myTasks",
-      templateUrl: "partials/userTasks.html"
-    })
-    .state('myTasks.list', {
-      url: "/listTasks",
-      templateUrl: "partials/myTasks.list.html",
-      controller: function($scope) {
-        $scope.tasks = ["A", "Set", "Of", "Tasks"];
-      }
-    });
+    //
+    // For any unmatched url, redirect to /login
+    $urlRouterProvider.otherwise("/login");
+    //
+    // Now set up the states
+    $stateProvider
+        .state('login', {
+            url: "/login",
+            templateUrl: "partials/login.html"
+        })
+        .state('myTasks', {
+            url: "/myTasks",
+            templateUrl: "partials/userTasks.html",
+            params: {
+                data: null
+            }
+        })
+        .state('myTasks.list', {
+            url: "/listTasks",
+            templateUrl: "partials/myTasks.list.html"
+        });
 });
+
+function ErrorCallback(err) {
+    console.log(err);
+}
